f9f039b6c4943e718af6ec55997bca2278fa4fe9
lekeux kevin <strodown@gmail.com>
2020-01-11 03:25:37 +0100

    chore(supermodel): add patch for config files and controllers
    chore(supermodel): bump to r784
    
    
    chore(supermodel): add patch for default controllers and bump emulator
    
    
    chore(supermodel): delete SED for add real patch and rework directory 
    
    
    fix(supermodel): correct missing letter

--- package/supermodel/supermodel.mk
+++ package/supermodel/supermodel.mk
@@ -4,14 +4,15 @@
 #
 ################################################################################
 
-SUPERMODEL_VERSION = r782
+SUPERMODEL_VERSION = r787
 SUPERMODEL_SITE = svn://svn.code.sf.net/p/model3emu/code/trunk
 SUPERMODEL_SITE_METHOD = svn
 SUPERMODEL_LICENSE = GPL2
-SUPERMODEL_DEPENDENCIES = sdl2 zlib libpng libogg libvorbis sdl_sound sdl2_net
+SUPERMODEL_DEPENDENCIES = sdl zlib libpng libogg libvorbis sdl_sound sdl_net
 
 define SUPERMODEL_BUILD_CMDS
 	cp $(@D)/Makefiles/Makefile.UNIX $(@D)/Makefile
+	$(SED) "s|-O2|-O3|g" $(@D)/Makefile
 	$(SED) "s|CC = gcc|CC = $(TARGET_CC)|g" $(@D)/Makefile
 	$(SED) "s|CXX = g++|CXX = $(TARGET_CXX)|g" $(@D)/Makefile
 	$(SED) "s|LD = gcc|LD = $(TARGET_CC)|g" $(@D)/Makefile
@@ -23,9 +24,18 @@ define SUPERMODEL_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/bin/supermodel \
 		$(TARGET_DIR)/usr/bin/supermodel
 	$(INSTALL) -D -m 0644 $(@D)/Config/Games.xml \
-		$(TARGET_DIR)/recalbox/share_init/system/configs/model3/games.xml
+		$(TARGET_DIR)/recalbox/share_init/system/configs/model3/Games.xml
 	$(INSTALL) -D -m 0644 $(@D)/Config/Supermodel.ini \
-		$(TARGET_DIR)/recalbox/share_init/system/configs/model3/supermodel.ini
+		$(TARGET_DIR)/recalbox/share_init/system/configs/model3/Supermodel.ini
+	mkdir $(TARGET_DIR)/recalbox/share_init/system/configs/model3/NVRAM
 endef
+
+define SUPERMODEL_LINE_ENDINGS_FIXUP
+	# DOS2UNIX Supermodel.ini and Main.cpp - patch system does not support different line endings
+	sed -i -E -e "s|\r$$||g" $(@D)/Config/Supermodel.ini
+	sed -i -E -e "s|\r$$||g" $(@D)/Src/OSD/SDL/Main.cpp
+endef
+
+SUPERMODEL_PRE_PATCH_HOOKS += SUPERMODEL_LINE_ENDINGS_FIXUP
 
 $(eval $(generic-package))
